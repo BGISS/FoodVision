9e9f65e51c8b46671f3deb9ef883b797ac6c6f6a
BGISS <bissgirish22@gmail.com>
2023-12-27 14:54:04 -0500

    first commit

---
+++ vite-project/src/MainPage.tsx
@@ -0,0 +1,10 @@
+import BlurredElipse from "./components/BlurredElipse";
+function MainPage(){
+    return(
+    <>
+    <div className="ellipse1"><BlurredElipse color="red"/></div>
+    </>
+    )
+}
+
+export default MainPage;
---
+++ vite-project/src/components/BlurredElipse.tsx
@@ -0,0 +1,10 @@
+import './BlurredElipse.css'
+
+function BlurredElipse({ color}:{color:string}){
+    const ellipseStyle = {
+      background: `${color}`,
+    };
+  
+    return <div className="blurred-ellipse" style={ellipseStyle}></div>;
+  };
+export default BlurredElipse;
